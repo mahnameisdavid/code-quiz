e54f7782bd59f4626d7d0638172c002a239b5c03
mahnameisdavid <dee.echeverrria@gmail.com>
2022-04-03 15:54:00 -0700

    html

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const buttonElement = document.querySelector("#startQuiz")
+
+/*
+
+TODO: When user clicks the start button
+Then we start the timer
+Then we need to show the list of quesitons
+
+TODO: When user selects the question
+Then verify to show the list of questions
+
+TODO: If the answe is correct
+We then verify if they have the correct answer or not\
+
+TODO: If the answe is wrong
+Then the next question will show and the timer will cut shorter
+
+TODO: if the timer went out or there are no more question the game is over
+we then give user the option to see their highscore and enter
+
+TODO: we then biild a seperate html with the users high score
+
+
+*/
