9f00635ce26d86c594171fe7a9ba91d6de901894
mahnameisdavid <dee.echeverrria@gmail.com>
2022-04-03 22:27:28 -0700

    main start page has been hidden, when start quiz button has been selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,75 @@
-const buttonElement = document.querySelector("#startQuiz")
+const startButton = document.querySelector("#startQuiz")
+
+startButton.addEventListener('click', startQuiz)
 
 var timer;
-var counter  = 60;
+var counter  = 10;
 var currentIterator = 0;
 
+
+// list of questions used for the quiz along with the choices and correct answers
 var listOfQuestions = [
     { question: "Commonly used DATA types do NOT include:", choices: ["Strings", "booleans", "alerts", "numbers"],
 correctAnswer: 2 },
-    { question: ""}
+    { question: "The condition in an if/else statement is enclosed with ______", choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+correctAnswer: 2 },
+    { question: "Arrays in Javascript can be used to store ______", choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+correctAnswer: 3 },
+    { question: "String values must be enclosed within ____ when being assigned to variables.", choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+correctAnswert: 2 },
+    { question: "A very useful tool used during development and debuggin for printing content to the debugger is:", choices: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. conosle.log"],
+    correctAnswer: 3 },
 ]
 
+varisGameOver = false;
+
+
+//start the game off and hide the screen
+function startQuiz() {
+    console.log('Quiz has begun');
+    var startScreenElement = document.getElementById("main-page");
+    startScreenElement.setAttribute("class", "hide");
+}
+
+// as the start question begins this timer will go until quiz is finished
 var startTimer = function () {
     timer = setInterval (() => {
         counter--;
         console.log("counter: ", counter)
 
         if (counter === 0) {
-            isGameover + true;
+            isGameover = true;
             stopTimer();
-            console.assertlog("ALL DONE!")
+            console.log("ALL DONE!")
         }
     }, 1000)
 }
 
-buttonElement.onclick = function () {
+
+var stopTimer = function () {
+
+    clearInterval(timer)
+}
+
+var nextQuestion = function () {
+    const currentIter = iterator;
+
+    const questionContainerEl = doucment.createElement("diy")
+
+    for (var i = 0; question.choices.length; i++) {
+
+    }
+
+
+}
+
+var showQuestion = function () {
+    const question = nextQuestion();
+
+    
+}
+
+startButton.onclick = function () {
     startTimer();
 }
 
